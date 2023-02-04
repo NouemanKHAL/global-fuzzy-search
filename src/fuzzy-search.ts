cd96c62cf0b3f8c7646204504f4599a2443d45e9
NouemanKHAL <noueman.khal@gmail.com>
2023-02-04 19:43:41 +0100

    format

--- src/fuzzy-search.ts
+++ src/fuzzy-search.ts
@@ -1,7 +1,11 @@
 import * as vscode from "vscode";
-import * as path from 'path';
-import { showSearchResults, SearchResultItem, SearchResultItemNode} from "./components";
-import { fastFindInFiles } from 'fast-find-in-files'
+import * as path from "path";
+import {
+  showSearchResults,
+  SearchResultItem,
+  SearchResultItemNode,
+} from "./components";
+import { fastFindInFiles } from "fast-find-in-files";
 
 // TODO: implement fuzzy search when the GUI work is done
 export async function fuzzySearchCommand() {
@@ -13,17 +17,33 @@ export async function fuzzySearchCommand() {
     return;
   }
 
-  const results = [];
-
   let entries = fastFindInFiles(process.cwd(), searchTerm);
+
+  const results = [];
 
   for (let entry of entries) {
     let filename = path.basename(entry.filePath);
     let rootItem = new SearchResultItem(entry.filePath, 0, "");
-    let rootNode = new SearchResultItemNode(rootItem, filename, vscode.TreeItemCollapsibleState.Collapsed, vscode.ThemeIcon.File, []); 
+    let rootNode = new SearchResultItemNode(
+      rootItem,
+      filename,
+      vscode.TreeItemCollapsibleState.Collapsed,
+      vscode.ThemeIcon.File,
+      []
+    );
     for (let hit of entry.queryHits) {
-      let childItem = new SearchResultItem(entry.filePath, hit.lineNumber, hit.line);
-      let childNode = new SearchResultItemNode(childItem, hit.line, vscode.TreeItemCollapsibleState.None, undefined, undefined);
+      let childItem = new SearchResultItem(
+        entry.filePath,
+        hit.lineNumber,
+        hit.line
+      );
+      let childNode = new SearchResultItemNode(
+        childItem,
+        hit.line,
+        vscode.TreeItemCollapsibleState.None,
+        undefined,
+        undefined
+      );
       rootNode.children?.push(childNode);
     }
     results.push(rootNode);

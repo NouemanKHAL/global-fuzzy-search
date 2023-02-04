f086b150a9acc1e5c914d4bf8b3861752791c071
NouemanKHAL <noueman.khal@gmail.com>
2023-02-04 22:50:13 +0100

    working fuzzy search

--- src/fuzzy-search.ts
+++ src/fuzzy-search.ts
@@ -1,12 +1,36 @@
 import * as vscode from "vscode";
 import * as path from "path";
+import { distance } from 'fastest-levenshtein';
+
 import {
   showSearchResults,
   SearchResultItem,
   SearchResultItemNode,
 } from "./components";
 
-import { fastFindInFiles } from "fast-find-in-files";
+
+class LineMatch {
+  constructor(public line: string, public lineNumber: number) {}
+}
+
+// TODO: consider splitting the pattern by spaces?
+function fuzzyMatch(fileContent: string, pattern: string): LineMatch[] {
+  const lines = fileContent.split("\n");
+
+  let results = [];
+
+  for (const [i, line] of lines.entries()) {
+    const words = line.split(" ");
+    for (const word of words) {
+      if (distance(word, pattern) <= 5) {
+          results.push(new LineMatch(line, i));
+          break;
+      }
+    }
+  }
+
+  return results;
+}
 
 // TODO: implement fuzzy search when the GUI work is done
 export async function fuzzySearchCommand() {
@@ -25,35 +49,52 @@ export async function fuzzySearchCommand() {
     cwd = process.cwd();
   }
 
-  let entries = fastFindInFiles(cwd, searchTerm);
-
   const results = [];
 
-  for (let entry of entries) {
-    let filename = path.basename(entry.filePath);
-    let rootItem = new SearchResultItem(entry.filePath, 0, "");
-    let rootNode = new SearchResultItemNode(
-      rootItem,
-      filename,
-      path.relative(cwd, entry.filePath),
-      []
-    );
-    for (let hit of entry.queryHits) {
-      let childItem = new SearchResultItem(
-        entry.filePath,
-        hit.lineNumber,
-        hit.line
+  const files = await vscode.workspace.findFiles('**/*');
+
+  for (const file of files) {
+    try {
+      const document = await vscode.workspace.openTextDocument(file);
+      const documentText = document.getText();
+      
+      let hits = fuzzyMatch(documentText, searchTerm);
+      if (hits.length === 0) {
+        continue;
+      }
+
+      let filename = path.basename(file.fsPath);
+      let rootItem = new SearchResultItem(file.fsPath, 0, "");
+      let rootNode = new SearchResultItemNode(
+        rootItem,
+        filename,
+        path.relative(cwd, file.fsPath),
+        []
       );
-      let childNode = new SearchResultItemNode(
-        childItem,
-        hit.line,
-        "",
-        undefined
-      );
-      rootNode.children?.push(childNode);
+
+
+      for (const hit of hits) {
+        let childItem = new SearchResultItem(
+          file.fsPath,
+          hit.lineNumber,
+          hit.line
+        );
+        let childNode = new SearchResultItemNode(
+          childItem,
+          hit.line,
+          "",
+          undefined
+        );
+        rootNode.children?.push(childNode);
+      }
+      results.push(rootNode);
+    } catch(error) {
+      console.error(`Error opening file: ${error}`);
+      continue;
     }
-    results.push(rootNode);
   }
+
+  console.debug(results);
 
   showSearchResults(results);
 }
